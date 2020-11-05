acd5e75941423bdfd8a257648701149cfcd52519
YPZ-24 <aylinypz@gmail.com>
2020-11-04 21:37:39 -0600

    Biseccion

---
+++ biseccion/biseccion/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Bisección
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Calcular = New System.Windows.Forms.Button()
+        Me.Salir = New System.Windows.Forms.Button()
+        Me.Salida = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.lf = New System.Windows.Forms.Label()
+        Me.la = New System.Windows.Forms.Label()
+        Me.lb = New System.Windows.Forms.Label()
+        Me.lc = New System.Windows.Forms.Label()
+        Me.tf = New System.Windows.Forms.TextBox()
+        Me.ta = New System.Windows.Forms.TextBox()
+        Me.tb = New System.Windows.Forms.TextBox()
+        Me.tc = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Limpiar = New System.Windows.Forms.Button()
+        CType(Me.Salida, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Calcular
+        '
+        Me.Calcular.Location = New System.Drawing.Point(520, 55)
+        Me.Calcular.Name = "Calcular"
+        Me.Calcular.Size = New System.Drawing.Size(75, 23)
+        Me.Calcular.TabIndex = 0
+        Me.Calcular.Text = "Calcular"
+        Me.Calcular.UseVisualStyleBackColor = True
+        '
+        'Salir
+        '
+        Me.Salir.Location = New System.Drawing.Point(520, 95)
+        Me.Salir.Name = "Salir"
+        Me.Salir.Size = New System.Drawing.Size(75, 23)
+        Me.Salir.TabIndex = 1
+        Me.Salir.Text = "Salir"
+        Me.Salir.UseVisualStyleBackColor = True
+        '
+        'Salida
+        '
+        Me.Salida.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Salida.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8})
+        Me.Salida.Location = New System.Drawing.Point(12, 201)
+        Me.Salida.Name = "Salida"
+        Me.Salida.Size = New System.Drawing.Size(510, 150)
+        Me.Salida.TabIndex = 2
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Indice"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 50
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "a"
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 50
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Xi"
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 50
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "b"
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 50
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "f(a)"
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 50
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "f(Xi)"
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 50
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "f(b)"
+        Me.Column7.Name = "Column7"
+        Me.Column7.Width = 50
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "error"
+        Me.Column8.Name = "Column8"
+        Me.Column8.Width = 50
+        '
+        'lf
+        '
+        Me.lf.AutoSize = True
+        Me.lf.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.lf.Location = New System.Drawing.Point(161, 55)
+        Me.lf.Name = "lf"
+        Me.lf.Size = New System.Drawing.Size(73, 13)
+        Me.lf.TabIndex = 3
+        Me.lf.Text = "Dame función"
+        '
+        'la
+        '
+        Me.la.AutoSize = True
+        Me.la.Location = New System.Drawing.Point(161, 85)
+        Me.la.Name = "la"
+        Me.la.Size = New System.Drawing.Size(44, 13)
+        Me.la.TabIndex = 4
+        Me.la.Text = "Dame a"
+        '
+        'lb
+        '
+        Me.lb.AutoSize = True
+        Me.lb.Location = New System.Drawing.Point(161, 118)
+        Me.lb.Name = "lb"
+        Me.lb.Size = New System.Drawing.Size(44, 13)
+        Me.lb.TabIndex = 5
+        Me.lb.Text = "Dame b"
+        '
+        'lc
+        '
+        Me.lc.AutoSize = True
+        Me.lc.Location = New System.Drawing.Point(161, 153)
+        Me.lc.Name = "lc"
+        Me.lc.Size = New System.Drawing.Size(63, 13)
+        Me.lc.TabIndex = 6
+        Me.lc.Text = "Dame cifras"
+        '
+        'tf
+        '
+        Me.tf.Location = New System.Drawing.Point(292, 48)
+        Me.tf.Name = "tf"
+        Me.tf.Size = New System.Drawing.Size(100, 20)
+        Me.tf.TabIndex = 7
+        '
+        'ta
+        '
+        Me.ta.Location = New System.Drawing.Point(292, 78)
+        Me.ta.Name = "ta"
+        Me.ta.Size = New System.Drawing.Size(100, 20)
+        Me.ta.TabIndex = 8
+        '
+        'tb
+        '
+        Me.tb.Location = New System.Drawing.Point(292, 111)
+        Me.tb.Name = "tb"
+        Me.tb.Size = New System.Drawing.Size(100, 20)
+        Me.tb.TabIndex = 9
+        '
+        'tc
+        '
+        Me.tc.Location = New System.Drawing.Point(292, 146)
+        Me.tc.Name = "tc"
+        Me.tc.Size = New System.Drawing.Size(100, 20)
+        Me.tc.TabIndex = 10
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.biseccion.My.Resources.Resources.expresion
+        Me.PictureBox1.Location = New System.Drawing.Point(552, 200)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(345, 167)
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'Limpiar
+        '
+        Me.Limpiar.Location = New System.Drawing.Point(520, 146)
+        Me.Limpiar.Name = "Limpiar"
+        Me.Limpiar.Size = New System.Drawing.Size(75, 23)
+        Me.Limpiar.TabIndex = 12
+        Me.Limpiar.Text = "Limpiar"
+        Me.Limpiar.UseVisualStyleBackColor = True
+        '
+        'Bisección
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1006, 450)
+        Me.Controls.Add(Me.Limpiar)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.tc)
+        Me.Controls.Add(Me.tb)
+        Me.Controls.Add(Me.ta)
+        Me.Controls.Add(Me.tf)
+        Me.Controls.Add(Me.lc)
+        Me.Controls.Add(Me.lb)
+        Me.Controls.Add(Me.la)
+        Me.Controls.Add(Me.lf)
+        Me.Controls.Add(Me.Salida)
+        Me.Controls.Add(Me.Salir)
+        Me.Controls.Add(Me.Calcular)
+        Me.Name = "Bisección"
+        Me.Text = "Método de Bisección"
+        CType(Me.Salida, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Calcular As Button
+    Friend WithEvents Salir As Button
+    Friend WithEvents Salida As DataGridView
+    Friend WithEvents lf As Label
+    Friend WithEvents la As Label
+    Friend WithEvents lb As Label
+    Friend WithEvents lc As Label
+    Friend WithEvents tf As TextBox
+    Friend WithEvents ta As TextBox
+    Friend WithEvents tb As TextBox
+    Friend WithEvents tc As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Limpiar As Button
+End Class
---
+++ biseccion/biseccion/Form1.vb
@@ -0,0 +1,70 @@
+﻿
+Imports info.lundin.math
+Public Class Bisección
+
+    Dim a As Single
+    Dim b As Single
+    Dim c As Integer
+    Dim x(50) As Single
+    Dim err(50) As Single
+    Dim ec As Single
+    Dim i As Integer
+    Dim redon As Integer
+
+    Function f(x As Single) As Single
+        Dim parser As ExpressionParser
+        parser = New ExpressionParser
+        parser.Values.Clear()
+        parser.Values.Add("x", x)
+        Return parser.Parse(tf.Text)
+
+    End Function
+    Private Sub Salir_Click(sender As Object, e As EventArgs) Handles Salir.Click
+        End
+    End Sub
+    Private Sub Calcular_Click(sender As Object, e As EventArgs) Handles Calcular.Click
+        a = ta.Text
+        b = tb.Text
+        c = tc.Text
+        ec = 0.5 * 10 ^ (-c)
+        redon = c + 2
+        x(i) = (a + b) / 2
+        err(i) = 1
+        Do While err(i) > ec
+            If f(a) * f(x(i)) < 0 Then
+                b = x(i)
+            Else
+                a = x(i)
+            End If
+
+            i = i + 1
+            x(i) = (a + b) / 2
+            err(i) = Math.Abs((x(i) - x(i - 1)) / x(i))
+
+            Salida.Rows.Add(i, Math.Round(a, redon), Math.Round(x(i), redon),
+         Math.Round(b, redon),
+         Math.Round(f(a), redon), Math.Round(f(x(i)), redon), Math.Round(f(b), redon),
+         Math.Round(err(i), redon))
+
+        Loop
+
+        Salida.Rows.Add("La raiz es ", " x = ", Math.Round(x(i), redon))
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Limpiar.Click
+        tf.Clear()
+        ta.Clear()
+        tb.Clear()
+        tc.Clear()
+        Salida.Rows.Clear()
+    End Sub
+End Class
